814552599d8f70350033a4d2a293c11db41a9bbb
dan-i <dani@dapulse.com>
2019-06-18 16:00:03 +0300

    Update gpt_2.py

--- gpt2_estimator/gpt2/gpt_2.py
+++ gpt2_estimator/gpt2/gpt_2.py
@@ -19,7 +19,8 @@ except:
     pass
 
 from gpt2.src import model, sample, encoder
-from gpt2.src.mqa_load_dataset import load_dataset, Sampler
+from gpt2.src.load_dataset import load_dataset
+import gpt2.src.sample as Sampler
 from gpt2.src.accumulate import AccumulatingOptimizer
 
 
